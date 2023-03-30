090a85c610aaaedc57cbbf1baec2c0a3244a28de
Jun <122006463+TiranoPower@users.noreply.github.com>
2023-03-30 18:00:16 +0900

    Update readme.d

--- Study/DL/NLP/실습/readme.d
+++ Study/DL/NLP/실습/readme.d
@@ -1,9 +1,12 @@
 출처 : suanlab.com, 이수안연구소
 
 
+
+
+* NLP
 1.자연어 처리 : https://www.youtube.com/watch?v=yW63iUxXnlc&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=18
 
-2.RNN : https://www.youtube.com/watch?v=EISIxi1LBjw&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=22
+2.텍스트 분석 https://www.youtube.com/watch?v=RgKzmJQf21s&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=19
 
 3.RNN 텍스트 분류 : https://www.youtube.com/watch?v=mX7-QVw3WW8&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=23
 
@@ -15,8 +18,11 @@
 
 7.BERT : https://www.youtube.com/watch?v=LEtLfx1dS7Q&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=16
 
-8.RNN 실습(NLP, Word Embedding, CBOW, Skip-Gram, Word2Vec, LSTM, GRU) : https://www.youtube.com/watch?v=r92QktoXsas
 
-9.CNN 스팸메일 분류 : https://www.youtube.com/watch?v=QejZQ0Dh5x8&t=370s
+* 기타
 
+
+RNN : https://www.youtube.com/watch?v=EISIxi1LBjw&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=22
+
+RNN 실습(NLP, Word Embedding, CBOW, Skip-Gram, Word2Vec, LSTM, GRU) : https://www.youtube.com/watch?v=r92QktoXsas
 
