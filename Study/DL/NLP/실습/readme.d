615b0fe8a310f45e3fe306cd81e9071d8708f4b2
Jun <122006463+TiranoPower@users.noreply.github.com>
2023-04-03 00:50:11 +0900

    Update readme.d

--- Study/DL/NLP/실습/readme.d
+++ Study/DL/NLP/실습/readme.d
@@ -6,12 +6,10 @@
 * NLP(4주 미프 준비 : 데이터 분석 - 데이터 전처리 - 머신러닝 - 딥러닝 - 캐글)
 1.자연어 처리 : https://www.youtube.com/watch?v=yW63iUxXnlc&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=18
 
-2.텍스트 분석 https://www.youtube.com/watch?v=RgKzmJQf21s&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=19
+2.RNN 텍스트 처리 및 분류 : https://www.youtube.com/watch?v=mX7-QVw3WW8&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=23
 
-3.RNN 텍스트 처리 및 분류 : https://www.youtube.com/watch?v=mX7-QVw3WW8&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=23
+3.Seq2Seq, 어텐션 메커니즘 : https://www.youtube.com/watch?v=1eLdaJ-Y7zM&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=24 
 
-4.Seq2Seq, 어텐션 메커니즘 : https://www.youtube.com/watch?v=1eLdaJ-Y7zM&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=24 
+4.GPT2 :https://www.youtube.com/watch?v=t43qcsVydnY&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=17
 
-6.GPT2 :https://www.youtube.com/watch?v=t43qcsVydnY&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=17
-
-7.BERT : https://www.youtube.com/watch?v=LEtLfx1dS7Q&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=16
+5.BERT : https://www.youtube.com/watch?v=LEtLfx1dS7Q&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=16
