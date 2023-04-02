1852cb247e368a8a72c92aecb4d90f6f237d598b
Jun <122006463+TiranoPower@users.noreply.github.com>
2023-04-03 00:48:40 +0900

    Update readme.d

--- Study/DL/NLP/실습/readme.d
+++ Study/DL/NLP/실습/readme.d
@@ -12,17 +12,9 @@
 
 4.Seq2Seq, 어텐션 메커니즘 : https://www.youtube.com/watch?v=1eLdaJ-Y7zM&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=24 
 
-5.Transformer : https://www.youtube.com/watch?v=Izi9trF3nKY&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=15
-
 6.GPT2 :https://www.youtube.com/watch?v=t43qcsVydnY&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=17
 
 7.BERT : https://www.youtube.com/watch?v=LEtLfx1dS7Q&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=16
 
 
 * 기타
-
-
-RNN : https://www.youtube.com/watch?v=EISIxi1LBjw&list=PL7ZVZgsnLwEEoHQAElEPg7l7T6nt25I3N&index=22
-
-RNN 실습(NLP, Word Embedding, CBOW, Skip-Gram, Word2Vec, LSTM, GRU) : https://www.youtube.com/watch?v=r92QktoXsas
-
